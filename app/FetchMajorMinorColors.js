1522db6d6741a21343b4a45cf2cfe5afaa8beb0e
pritishg3 <ghate.pritish3@gmail.com>
2022-05-23 18:51:01 +0530

    Fixed path issue

--- app/FetchMajorMinorColors.js
+++ app/FetchMajorMinorColors.js
@@ -3,7 +3,7 @@ let fs = require("fs");
 class FetchMajorMinorColors {
   constructor() {
     this.data = fs.readFileSync(
-      __dirname + "\\assets\\color_names.json",
+      __dirname + "/assets/color_names.json",
       "utf8"
     );
     this.data = JSON.parse(this.data);
